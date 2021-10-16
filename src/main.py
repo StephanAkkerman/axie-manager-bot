c46f77d3d2837ef0abbb6fa10b3edcf445c31769
Akkie100 <stephan.akkerman@gmail.com>
2021-10-16 15:50:56 +0200

    Is now a bot instead of client :rocket:

--- src/main.py
+++ src/main.py
@@ -1,152 +1,142 @@
+#!/usr/bin/env python3
+
+##> Imports
+#> Standar library
 import os
+from datetime import datetime
+
+#> 3rd Party Dependencies
 import discord
+from discord.ext import commands
+from cryptography.fernet import Fernet
 import uuid
+
+#> Local Dependencies
 from SecretStorage import *
 from QRCodeBot import *
-from datetime import datetime
-from cryptography.fernet import Fernet
-
-# Set the client intents
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
 
 # Get encryption key
 fernet = Fernet(KEY)
 
-# Todo
-# bot = commands.Bot(command_prefix='!')
-# Add @bot.command() instead of on_message
+# Bot prefix is !
+bot = commands.Bot(command_prefix='!',  intents=discord.Intents.all())
 
-
-@client.event
+@bot.event
 async def on_ready():
     """ This gets printed on boot up"""
 
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
     print(
-        f"{client.user} is connected to the following guild:\n"
+        f"{bot.user} is connected to the following guild:\n"
         f"{guild.name}(id: {guild.id})"
     )
 
-
-# @client.event
+# Make this a toggle??
+# @bot.event
 # async def on_member_join(member):
 #    """ Gives new users the role Tryout """
 #    role = discord.utils.get(member.guild.roles, name="Tryout")
 #    await member.add_roles(role)
 
 # QR code bot
-@client.event
-async def on_message(message):
-    """ Listen for any incoming message """
-
-    # If the author is the robot itself, then do nothing!
-    if message.author == client.user:
-        return
+@bot.command()
+async def qr(ctx):
+    """ 
+    Give users their qr code if they request it in the correct channel.
+    Discord ID needs to be linked to Ronin Address and fernet encrypted privte key
+    """
 
     # If the user writes !qr in the correct channel
-    if message.channel.name == "🤖┃login":
+    if ctx.channel.name == "🤖┃login":
 
         # Delete this message, to remove clutter
-        await message.delete()
+        await ctx.message.delete()
 
-        # Do the rest if message is !qr
-        if message.content == "!qr":
+        # For logs
+        current_time = datetime.now().strftime("%H:%M:%S")
+        print("\n")
 
-            # For logs
-            current_time = datetime.now().strftime("%H:%M:%S")
-            print("\n")
+        # Get this scholars info from the sheet
+        scholar_info = getScholar(ctx.message.author.id)
 
-            # Get this scholars info from the sheet
-            scholar_info = getScholar(message.author.id)
+        # This for loop check for all the user's DiscordID in the Database
+        if scholar_info != None:
 
-            # This for loop check for all the user's DiscordID in the Database
-            if scholar_info != None:
+            # Print log info
+            print("This user received his QR Code : " + ctx.message.author.name)
+            print("Discord ID : " + str(ctx.message.author.id))
+            print("Current time : ", current_time)
 
-                # Print log info
-                print("This user received his QR Code : " + message.author.name)
-                print("Discord ID : " + str(message.author.id))
-                print("Current time : ", current_time)
+            # discordID's encrypted privateKey from the sheets, as a string
+            encrypted_key = scholar_info[1][2:-1]
 
-                # discordID's encrypted privateKey from the sheets, as a string
-                encrypted_key = scholar_info[1][2:-1]
+            # Convert it to bytes and decrypt it, remove 0x
+            accountPrivateKey = fernet.decrypt(
+                encrypted_key.encode("utf_8")
+            ).decode()[2:]
 
-                # Convert it to bytes and decrypt it, remove 0x
-                accountPrivateKey = fernet.decrypt(
-                    encrypted_key.encode("utf_8")
-                ).decode()[2:]
+            # discordID's Ronin wallet from the database, replace ronin: with 0x
+            accountAddress = scholar_info[0].replace("ronin:", "0x")
 
-                # discordID's Ronin wallet from the database, replace ronin: with 0x
-                accountAddress = scholar_info[0].replace("ronin:", "0x")
+            # Get a message from AxieInfinty
+            rawMessage = getRawMessage()
 
-                # Get a message from AxieInfinty
-                rawMessage = getRawMessage()
+            # Sign that message with accountPrivateKey
+            signedMessage = getSignMessage(rawMessage, accountPrivateKey)
 
-                # Sign that message with accountPrivateKey
-                signedMessage = getSignMessage(rawMessage, accountPrivateKey)
+            # Get an accessToken by submitting the signature to AxieInfinty
+            accessToken = submitSignature(signedMessage, rawMessage, accountAddress)
 
-                # Get an accessToken by submitting the signature to AxieInfinty
-                accessToken = submitSignature(signedMessage, rawMessage, accountAddress)
+            # Create a QrCode with that accessToken
+            qrCodePath = f"QRCode_{ctx.message.author.id}_{str(uuid.uuid4())[0:8]}.png"
+            qrcode.make(accessToken).save(qrCodePath)
 
-                # Create a QrCode with that accessToken
-                qrCodePath = f"QRCode_{message.author.id}_{str(uuid.uuid4())[0:8]}.png"
-                qrcode.make(accessToken).save(qrCodePath)
-
-                # Send the QrCode the the user who asked for it
-                await message.author.send(
-                    "------------------------------------------------\n\n\nHello "
-                    + message.author.name
-                    + "\nHere is your new QR Code to login : "
-                )
-                await message.author.send(file=discord.File(qrCodePath))
-                # Delete picture
-                os.remove(qrCodePath)
-                return
-
-            # Scholar does not exist in sheet, maybe different Discord ID?
-            else:
-                print("This user didn't receive a QR Code : " + message.author.name)
-                print("Discord ID : " + str(message.author.id))
-                print("Current time : ", current_time)
-                return
-
-        # Send feedback if it's not !qr
-        else:
-            await message.author.send(
-                "Please only send '!qr' in the login channel channel!"
+            # Send the QrCode the the user who asked for it
+            await ctx.message.author.send(
+                "------------------------------------------------\n\n\nHello "
+                + ctx.message.author.name
+                + "\nHere is your new QR Code to login : "
             )
-
-    # ==================
-    # === ENCRYPTION ===
-    # ==================
-
-    # If the user writes !encrypt followed by something else
-    if message.channel.name == "🤖┃bots":
-        if message.content.split()[0] == "!encrypt":
-
-            # Error handling
-            if len(message.content.split()) == 1:
-                await message.author.send("Please type something after !encrypt")
-                return
-
-            # Delete this message, for privacy
-            await message.delete()
-
-            # Private key is send after !encrypt
-            private_key = message.content.split()[1]
-
-            # Encode private key
-            encMessage = fernet.encrypt(private_key.encode())
-
-            # Send a dm
-            await message.author.send(
-                "Hi "
-                + message.author.name
-                + ", here is your encrypted key:\n"
-                + str(encMessage)
-            )
+            await ctx.message.author.send(file=discord.File(qrCodePath))
+            # Delete picture
+            os.remove(qrCodePath)
             return
 
+        # Scholar does not exist in sheet, maybe different Discord ID?
+        else:
+            print("This user didn't receive a QR Code : " + ctx.message.author.name)
+            print("Discord ID : " + str(ctx.message.author.id))
+            print("Current time : ", current_time)
+            return
 
-client.run(TOKEN)
+@bot.command()
+async def encrypt(ctx, message=None):
+    """
+    Encrypts a user's message using your personal fernet key
+    Message following !encrypt should not be empty
+    """
+
+    # If the user writes !encrypt followed by something else
+    if ctx.channel.name == "🤖┃bots":
+
+        # Error handling
+        if message == None:
+            await ctx.message.author.send("Please type something after !encrypt")
+            return
+
+        # Delete this message, for privacy
+        await ctx.message.delete()
+
+        # Encode private key
+        encMessage = fernet.encrypt(message.encode())
+
+        # Send a dm
+        await ctx.message.author.send(
+            "Hi "
+            + ctx.message.author.name
+            + ", here is your encrypted key:\n"
+            + str(encMessage)
+        )
+        return
+
+bot.run(TOKEN)
