caa4dacd8467914e4504767b01c5418f22c4af96
Akkie100 <stephan.akkerman@gmail.com>
2021-10-20 12:43:32 +0200

    Now includes stats for every axie

--- src/alerts/genes.py
+++ src/alerts/genes.py
@@ -2,13 +2,17 @@ import requests
 import pandas as pd
 
 
-def get_genes(axie_df, r1, r2):
+def get_genes(axie_df, r1, r2, add_info=False):
 
     # Get all axie ids and add them together
     ids = ",".join(axie_df["id"].tolist())
 
     try:
-        response = requests.get("https://api.axie.technology/getgenes/" + ids).json()
+        if add_info:
+            # If we need to add stats and auction info
+            response = requests.get("https://api.axie.technology/getgenes/" + ids + "/all").json()
+        else:
+            response = requests.get("https://api.axie.technology/getgenes/" + ids).json()
 
     except Exception as e:
         print(e)
@@ -18,6 +22,16 @@ def get_genes(axie_df, r1, r2):
     # Eyes and ears not important
     genes = pd.DataFrame(response)
 
+    if add_info:
+        
+        # Add stats and auction info to axie_df, has the same order as axie_df
+        # Does this cause this warning: A value is trying to be set on a copy of a slice from a DataFrame.??
+        axie_df[['stats','auction_info']] = genes[['stats', 'auction']].to_numpy()
+
+        # Add columns for parts
+        for part in ["mouth", "horn", "back", "tail"]:
+            genes[part] = genes["traits"].apply(lambda x: x[part])
+
     # Count deviations for every part
     for part in ["mouth", "horn", "back", "tail"]:
         if len(axie_df) == 1:
@@ -26,8 +40,13 @@ def get_genes(axie_df, r1, r2):
             genes[f"{part} r2"] = 0 if genes.iloc[0][part] == genes.iloc[2][part] else 1
 
         else:
-            genes[f"{part} r1"] = [0 if x["d"] == x["r1"] else 1 for x in genes[part]]
-            genes[f"{part} r2"] = [0 if x["d"] == x["r2"] else 1 for x in genes[part]]
+            if add_info:
+                genes[f"{part} r1"] = [0 if x["d"] == x["r1"] else 1 for x in genes[part]]
+                genes[f"{part} r2"] = [0 if x["d"] == x["r2"] else 1 for x in genes[part]]
+
+            else:
+                genes[f"{part} r1"] = [0 if x["d"] == x["r1"] else 1 for x in genes[part]]
+                genes[f"{part} r2"] = [0 if x["d"] == x["r2"] else 1 for x in genes[part]]
 
     # Sum all the deviations
     genes["r1 deviation"] = (
@@ -41,7 +60,10 @@ def get_genes(axie_df, r1, r2):
     genes = genes.loc[(genes["r1 deviation"] <= r1) & (genes["r2 deviation"] <= r2)]
 
     # Get the corresponding ids
-    ids = genes["axieId"].tolist()
+    if add_info:
+        ids = genes["story_id"].tolist()
+    else:
+        ids = genes["axieId"].tolist()
 
     # Trim the axie_df based on those ids
     axie_df = axie_df.loc[axie_df["id"].isin(ids)]
