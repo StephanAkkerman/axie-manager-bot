d97c610bb0cfb01294f4fc10fc7649135b8867d6
Akkie100 <stephan.akkerman@gmail.com>
2021-11-16 18:39:43 +0100

    Needs API from api.py and to fix send_warning

--- src/cogs/after/tasks.py
+++ src/cogs/after/tasks.py
@@ -2,11 +2,17 @@ import asyncio
 from datetime import datetime, timedelta
 
 from dateutil.relativedelta import relativedelta
+import gspread
+import gspread_dataframe as gd
+import pandas as pd
 
 import discord
 from discord.ext import commands
 from discord.ext.tasks import loop
 
+from alerts.api import 
+
+gc = gspread.service_account(filename="authentication.json")
 class Tasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -27,6 +33,34 @@ class Tasks(commands.Cog):
 
     @loop(hours=168)
     async def slp_warning(self):
+        scholar_info = (
+            gd.get_as_dataframe(gc.open("Scholars").worksheet("Scholars")).dropna(axis=0, how="all").dropna(axis=1, how="all")
+        )
+
+        # Get all addresses and join together as a string seperated by commas
+        together = ",".join(scholar_info["Address"].tolist())
+
+        # Call all addresses at once and retreive json
+        response = requests.get(
+            "https://game-api.axie.technology/api/v1/" + together
+        ).json()
+
+        df = pd.DataFrame(response).transpose().reset_index()
+
+        df['days_since_last_claim'] = (datetime.now() - pd.to_datetime(df['last_claim'],unit='s'))
+        df['days_since_last_claim'] = df['days_since_last_claim'].apply(lambda x: x.days if x.days > 0 else 1)
+        df['avg_slp'] = df['in_game_slp'] / df['days_since_last_claim']
+
+        bad_scholars = df.loc[df['avg_slp'] < 100]
+
+        bad_addresses = bad_scholars['index'].str.replace('0x', 'ronin:')
+
+        for address in bad_addresses:
+            discord_id = scholar_info.loc[scholar_info['Address'] == address]['Scholar Discord ID'].tolist()[0]
+            
+            await send_warning(discord_id)
+            
+    async def send_warning(discord_id):
         pass
 
     @slp_warning.before_loop
