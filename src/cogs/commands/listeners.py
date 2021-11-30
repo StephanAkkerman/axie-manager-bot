4727c5b71940fa0830604e00e7ba6eda6099bcf9
Akkie100 <stephan.akkerman@gmail.com>
2021-11-30 22:20:18 +0100

    Added tryout listener

--- src/cogs/commands/listeners.py
+++ src/cogs/commands/listeners.py
@@ -11,6 +11,13 @@ class Listeners(commands.Cog):
     @commands.Cog.listener(name="on_command")
     async def print(self, ctx):
         print(f"{ctx.author} used !{ctx.command} in channel {ctx.message.channel}")
+        
+    # On maybe join
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+       """ Gives new users the role Tryout """
+       role = discord.utils.get(member.guild.roles, name="Tryout")
+       await member.add_roles(role)
 
     ###################
     #### REACTIONS ####
